28c3db8e7a4f449f28936c4c06c9b99d1f251592
Paulo Adeva <paulo.adeva@gmail.com>
2022-12-29 22:03:41 +1100

    almost finished the features layout

--- src/header.js
+++ src/header.js
@@ -3,17 +3,27 @@ const content = document.querySelector('.content');
 function header() {
     const header = document.createElement('div');
     header.className = "header";
-
     content.appendChild(header);
 
     const title = document.createElement('div');
     title.className = "title";
-
-    title.innerText = "Remembrall";
-
+    title.innerText = "remembrall";
     header.appendChild(title);
+
+    const headerButtons = document.createElement('div');
+    headerButtons.className = "headerButtons";
+    header.appendChild(headerButtons);
+
+    const user = document.createElement('div');
+    user.className = "user";
+    headerButtons.appendChild(user);
+
+    const theme = document.createElement('div');
+    theme.className = "theme";
+    headerButtons.appendChild(theme);
 
 }
 header();
+
 
 export {header};
--- src/sidebar.js
+++ src/sidebar.js
@@ -3,8 +3,46 @@ const content = document.querySelector('.content');
 function sidebar() {
     const sidebar = document.createElement('div');
     sidebar.className = "sidebar";
-
     content.appendChild(sidebar);
+
+    const today = document.createElement('div');
+    sidebar.appendChild(today);
+
+    const todayIcon = document.createElement('span');
+    todayIcon.className = "material-symbols-outlined";
+    todayIcon.innerText = "today";
+    today.appendChild(todayIcon);
+
+    const todayText = document.createElement('div');
+    todayText.className = "todayText";
+    todayText.innerText = "Today";
+    today.appendChild(todayText);
+
+    const upcoming = document.createElement('div');
+    sidebar.appendChild(upcoming);
+
+    const upcomingIcon = document.createElement('span');
+    upcomingIcon.className = "material-symbols-outlined";
+    upcomingIcon.innerText = "upcoming";
+    upcoming.appendChild(upcomingIcon);
+
+    const upcomingText = document.createElement('div');
+    upcomingText.className = "upcomingText";
+    upcomingText.innerText = "Upcoming";
+    upcoming.appendChild(upcomingText);
+
+    const projects = document.createElement('div');
+    sidebar.appendChild(projects);
+
+    const projectsIcon = document.createElement('span');
+    projectsIcon.className = "material-symbols-outlined";
+    projectsIcon.innerText = "build";
+    projects.appendChild(projectsIcon);
+
+    const projectsText = document.createElement('div');
+    projectsText.className = "projectsText";
+    projectsText.innerText = "Projects";
+    projects.appendChild(projectsText);
 }
 sidebar();
 
